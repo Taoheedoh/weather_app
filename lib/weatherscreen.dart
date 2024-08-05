f58c826c98042116dbd00abb42ea698597e8e4df
Taoheedoh <taoheedoh@gmail.com>
2024-08-05 20:59:05 +0100

    Add AppBar

--- lib/weatherscreen.dart
+++ lib/weatherscreen.dart
@@ -5,6 +5,14 @@ class WeatherScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Weather App',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
